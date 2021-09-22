796039e6af9cc4db87129953ae2160cde43a2cb3
galen <galenliu@live.com>
2021-09-23 00:37:54 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 import './App.css';
 import Nav from "./components/Nav";
 import React, {useState} from "react";
-import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import {BrowserRouter as Router, Route, useLocation, Switch, useParams} from "react-router-dom";
 import Settings from "./views/Settings"
 import {createTheme, ThemeProvider} from "@mui/material/styles";
 import {Theme} from "./components/Theme"
+import SignInSide from "./views/SignIn";
+import SignUp from "./views/SignUp";
 
 export const AppContext = React.createContext({})
 const theme = createTheme(Theme);
 
+
+
 function App() {
 
+
     const [drawerOpen, setDrawerOpen] = useState(false)
+    const [navOpen,setNavOpen] = useState(true)
 
     return (
         <AppContext.Provider value={{
@@ -21,13 +27,19 @@ function App() {
             <ThemeProvider theme={theme}>
                 <div className="App">
                     <Router>
-                        <Nav/>
+                        {navOpen && <Nav/>}
                         <div>
                             {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
                             <Switch>
                                 <Route exact path="/things">
                                     <h1>Things</h1>
+                                </Route>
+                                <Route exact path="/register">
+                                  <SignUp/>
+                                </Route>
+                                <Route exact path="/login">
+                                    <SignInSide/>
                                 </Route>
                                 <Route exact path="/settings">
                                     <Settings/>
