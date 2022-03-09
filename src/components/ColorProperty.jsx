5bcab81f3953302a6e5bca16bfb9733262b10ed5
galenliu <galenliu@live.com>
2022-03-09 11:04:46 +0800

    change

---
+++ src/components/ColorProperty.jsx
@@ -0,0 +1,16 @@
+import React, {useEffect, useState} from "react"
+import {HexColorPicker} from "react-colorful";
+
+
+export default function ColorProperty(props) {
+    const {property} = props
+    const [color, setColor] = useState(property.value)
+
+    useEffect(()=>{
+
+    },[color])
+
+    return (
+        <HexColorPicker color={color} onChange={setColor}/>
+    )
+}
