71ebe13e054e4d6783e01c6d1ecff115ed6c50c5
galenliu <galenliu@live.com>
2022-04-14 13:21:53 +0800

    change

--- src/js/capability/use-on-off-switch.jsx
+++ src/js/capability/use-on-off-switch.jsx
@@ -14,7 +14,7 @@ export default function useOnOffSwitch(description) {
     //description新建一个OnOffSwitch类
     const thing = new OnOffSwitch(thingModel, description, null)
 
-    const onProperty = useProperty(thing.model, thing.displayedProperties[thing?.onProperty], 50)
+    const onProperty = useProperty(thingModel, thing.displayedProperties[thing?.onProperty], 50)
 
     if (onProperty === null) {
         return
